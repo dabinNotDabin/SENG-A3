6d8b9fee9358e8f2e3749a0b68f04504ac42143a
dabinNotDabin <dabin.lamming@outlook.com>
2017-02-18 11:56:33 -0700

    Add files via upload

--- UnitTestProject1/BasicValidFunctionality.cs
+++ UnitTestProject1/BasicValidFunctionality.cs
@@ -119,21 +119,16 @@ namespace UnitTestProject1
             //Get Actual Unload
             var coinRacks = VMs[0].CoinRacks;
             foreach (var item in coinRacks)
-            {
                 actualUnload.CoinsInCoinRacks.Add(item.Unload());
-            }
+            
 
             var popCanRacks = VMs[0].PopCanRacks;
             foreach (var item in popCanRacks)
-            {
                 actualUnload.PopCansInPopCanRacks.Add(item.Unload());
-            }
 
             var unload = VMs[0].StorageBin.Unload();
             foreach (var item in unload)
-            {
                 actualUnload.PaymentCoinsInStorageBin.Add(item);
-            }
 
             //Set Expected Unload
             var expectedCoinList = new List<Coin>() { five, ten, two5, two5, two5, two5, hund, hund };
@@ -159,10 +154,10 @@ namespace UnitTestProject1
 
             //Reset expected unload
             expectedUnload = new VendingMachineStoredContents(); 
-            var expectedCoinList2 = new List<Coin>() { five, ten, two5, two5, two5, two5, hund, hund };
-            var expectedPopList2 = new List<PopCan>() { water, stuff };
-            expectedUnload.CoinsInCoinRacks.Add(expectedCoinList2);
-            expectedUnload.PopCansInPopCanRacks.Add(expectedPopList2);
+            expectedCoinList = new List<Coin>() { five, ten, two5, two5, two5, two5, hund, hund };
+            expectedPopList = new List<PopCan>() { water, stuff };
+            expectedUnload.CoinsInCoinRacks.Add(expectedCoinList);
+            expectedUnload.PopCansInPopCanRacks.Add(expectedPopList);
 
             //Reverse the inputs to the function call to ensure neither expected nor actual contain different elements from eachother
             //Remove all items from actual unload that are also in expected unload, return the difference (if any)
@@ -176,44 +171,6 @@ namespace UnitTestProject1
                 Assert.IsTrue(popList.Count == 0);
 
             Assert.IsTrue(unaccountedForUnload.PaymentCoinsInStorageBin.Count == 0);
-
-
-
-
-
-            /* /////////Useful for printing items in unload to debug our tests///////////
-             * /////////Each Test has a corresponding output (link in the Test explorer -- left -- near the bottom)
-
-                        foreach (var list in unload.CoinsInCoinRacks)
-                        {
-                            foreach (var item in list)
-                            {
-                                Console.WriteLine(item.Value);
-                            }
-                        }
-
-
-                        foreach (var list in unload.PopCansInPopCanRacks)
-                        {
-                            foreach (var item in list)
-                            {
-                                Console.WriteLine(item.Name);
-                            }
-                        }
-
-
-                        foreach (var item in unload.PaymentCoinsInStorageBin)
-                        {
-                            Console.WriteLine(item.Value);
-                        }
-
-
-                        Console.WriteLine("------------------------");
-
-            // *//////////////////////////////////////////////////////////////////////////
-
-
-
 
         }
 
@@ -256,7 +213,113 @@ namespace UnitTestProject1
 
 
 
-        //////////etc..
+
+
+        /*//////////////////////////////////////////////////////
+        * Test_PressWithNoCredits
+        *    Mimicks : T04-good-press-without-insert
+        * 
+        * Tests the scenario where;
+        *    No coins are inserted and a button is dispensed.
+        *    No change is dispensed.
+        *///////////////////////////////////////////////////////
+        [TestMethod]
+        public void Test_PressWithNoCredits()
+        {
+            int[] coinKinds = new int[4] { 5, 10, 25, 100 };
+
+            int[] costs = new int[3] { 250, 250, 205 };
+            List<int> popCosts = new List<int>(costs);
+
+            string[] names = new string[3] { "Coke", "water", "stuff" };
+            List<string> popNames = new List<string>(names);
+
+            int[] coinsToLoad = new int[4] { 1, 1, 2, 0 };
+            int[] popsToLoad = new int[3] { 1, 1, 1, };
+
+
+            //Mimic Test Script Calls
+            var vm = new VendingMachine(coinKinds, 3, 10, 10, 10);
+            VMs.Add(vm);
+            new VendingMachineLogic(vm);
+
+            VMs[0].Configure(popNames, popCosts);
+            VMs[0].LoadCoins(coinsToLoad);
+            VMs[0].LoadPopCans(popsToLoad);
+            VMs[0].SelectionButtons[0].Press();
+
+
+            //Get Actual Delivery and set Expected Delivery
+            delivered = new List<IDeliverable>(VMs[0].DeliveryChute.RemoveItems());
+
+            //Compare Actual Delivery to Exepected Delivery
+            foreach (var item in expected)
+            {
+                Assert.IsTrue(checker.contains(delivered, item));
+                checker.remove(delivered, item);
+            }
+
+            Assert.IsTrue(delivered.Count == 0);
+
+
+
+            //Get Actual Unload
+            var coinRacks = VMs[0].CoinRacks;
+            foreach (var item in coinRacks)
+                actualUnload.CoinsInCoinRacks.Add(item.Unload());
+
+
+            var popCanRacks = VMs[0].PopCanRacks;
+            foreach (var item in popCanRacks)
+                actualUnload.PopCansInPopCanRacks.Add(item.Unload());
+
+            var unload = VMs[0].StorageBin.Unload();
+            foreach (var item in unload)
+                actualUnload.PaymentCoinsInStorageBin.Add(item);
+
+            //Set Expected Unload
+            var expectedCoinList = new List<Coin>() { five, ten, two5, two5 };
+            var expectedPopList = new List<PopCan>() { coke, water, stuff };
+            expectedUnload.CoinsInCoinRacks.Add(expectedCoinList);
+            expectedUnload.PopCansInPopCanRacks.Add(expectedPopList);
+
+
+            //Compare Actual Unload with Expected Unload -- See Function Documentation for "checkUnload" in Checker.cs
+            //--------------------------------------------------------------------------------------------------------
+            //Remove all items from expected unload that are also in actual unload, return the difference (if any)
+            unaccountedForUnload = checker.checkUnload(expectedUnload, actualUnload);
+
+            //Assert that the difference is an empty set
+            foreach (var coinList in unaccountedForUnload.CoinsInCoinRacks)
+                Assert.IsTrue(coinList.Count == 0);
+
+            foreach (var popList in unaccountedForUnload.PopCansInPopCanRacks)
+                Assert.IsTrue(popList.Count == 0);
+
+            Assert.IsTrue(unaccountedForUnload.PaymentCoinsInStorageBin.Count == 0);
+
+
+            //Reset expected unload
+            expectedUnload = new VendingMachineStoredContents();
+            expectedCoinList = new List<Coin>() { five, ten, two5, two5 };
+            expectedPopList = new List<PopCan>() { coke, water, stuff };
+            expectedUnload.CoinsInCoinRacks.Add(expectedCoinList);
+            expectedUnload.PopCansInPopCanRacks.Add(expectedPopList);
+
+            //Reverse the inputs to the function call to ensure neither expected nor actual contain different elements from eachother
+            //Remove all items from actual unload that are also in expected unload, return the difference (if any)
+            unaccountedForUnload = checker.checkUnload(actualUnload, expectedUnload);
+
+            //Assert that the difference is an empty set
+            foreach (var coinList in unaccountedForUnload.CoinsInCoinRacks)
+                Assert.IsTrue(coinList.Count == 0);
+
+            foreach (var popList in unaccountedForUnload.PopCansInPopCanRacks)
+                Assert.IsTrue(popList.Count == 0);
+
+            Assert.IsTrue(unaccountedForUnload.PaymentCoinsInStorageBin.Count == 0);
+
+        }
 
 
 
